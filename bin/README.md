86e7d8fb536c59ff8bf76d05efbc18156807dbed
owenwikel <wikelogw@gmail.com>
2026-09-17 18:49:36 -0400

    fix project config

---
+++ bin/README.md
@@ -0,0 +1,2 @@
+# cs2114-project1-groupNN
+Customizable outfit curator based on your own wardrobe. 
--- gearedGarmentGenerator/Person.java
+++ gearedGarmentGenerator/Person.java
@@ -1,9 +1,18 @@
 package gearedGarmentGenerator;
+import java.util.ArrayList;
 public class Person {
     //~ Fields ................................................................
+   // private ArrayList<Top> topsList;
+   // private ArrayList<Bottom> bottomsList = new ArrayList<Bottom>();
+   // private ArrayList <Shoes> shoesList = new ArrayList<Shoes>();
+   // private ArrayList<Full> fullList = new ArrayList<Full>();
+   // private ArrayList<Jacket> jacketsList = new ArrayList<Jacket>();
+   // private String name;
 
     //~ Constructors ..........................................................
-
+    public Person() {
+        //topsList = new ArrayList<Top>();
+    }
     //~Public  Methods ........................................................
 
     public void name() {
