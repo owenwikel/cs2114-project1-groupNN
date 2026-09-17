8ec234f95f1291dbb7f6086dcc76cd714d354868
sydneyang111 <sydneyangg@gmail.com>
2026-09-17 17:13:18 -0400

    Update Sydney.java

--- Sydney.java
+++ Sydney.java
@@ -1,5 +1,5 @@
 public class Sydney {
     public static void main(String[] args) {
-        System.out.println("Sydney is on the team.");
+        System.out.println("Sydney is on the team!");
     }
 }
