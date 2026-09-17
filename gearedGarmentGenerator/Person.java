8719979f9f9213358fc3daee607098e5e8783c93
owenwikel <wikelogw@gmail.com>
2026-09-17 19:20:26 -0400

    person changes

--- gearedGarmentGenerator/Person.java
+++ gearedGarmentGenerator/Person.java
@@ -2,20 +2,27 @@ package gearedGarmentGenerator;
 import java.util.ArrayList;
 public class Person {
     //~ Fields ................................................................
-   // private ArrayList<Top> topsList;
-   // private ArrayList<Bottom> bottomsList = new ArrayList<Bottom>();
-   // private ArrayList <Shoes> shoesList = new ArrayList<Shoes>();
-   // private ArrayList<Full> fullList = new ArrayList<Full>();
+   private String name;
+   private ArrayList<Top> topsList;
+   private ArrayList<Bottom> bottomsList = new ArrayList<Bottom>();
+   private ArrayList <Shoes> shoesList = new ArrayList<Shoes>();
+   private ArrayList<Full> fullList = new ArrayList<Full>();
    // private ArrayList<Jacket> jacketsList = new ArrayList<Jacket>();
    // private String name;
 
     //~ Constructors ..........................................................
-    public Person() {
-        //topsList = new ArrayList<Top>();
+    public Person(String name) {
+        name = this.name
+        topsList = new ArrayList<Top>();
     }
     //~Public  Methods ........................................................
 
-    public void name() {
-        System.out.println("Hello");
+    public String getName(String name) {
+        return this.name;
     }
+    
+    public void welcomeUser() {
+        
+    }
+
 }
