bdaa7db5cc168fd4e9436c1e7348f5f90934801e
sydneyang111 <sydneyangg@gmail.com>
2026-09-23 21:11:22 -0400

    fixed full test, outfit and outfit test

--- gearedGarmentGenerator/Outfit.java
+++ gearedGarmentGenerator/Outfit.java
@@ -1,26 +1,24 @@
 package gearedGarmentGenerator;
+
 /**
  * // -------------------------------------------------------------------------
-/**
- *  Complete outfit consisting of the users individual clothing pieces
+ * /** Complete outfit consisting of the users individual clothing pieces
  * 
- *  @author sydneyyang
- *  @version Sep 22, 2026
+ * @author sydneyyang
+ * @version Sep 22, 2026
  */
 public class Outfit
 {
-    //~ Fields ................................................................
+    // ~ Fields ................................................................
     private Top top;
     private Bottom bottom;
     private Shoes shoes;
     private Jacket jacket;
     private Full full;
 
-    //~ Constructors ..........................................................
-    /*
-     * // ----------------------------------------------------------
+    // ~ Constructors ..........................................................
     /**
-     * Create a new Outfit object.
+     * Creates new outfit
      * 
      * @param top
      * @param bottom
@@ -28,134 +26,188 @@ public class Outfit
      * @param jacket
      * @param full
      */
-    public Outfit(Top top, Bottom bottom, Shoes shoes, Jacket jacket, Full full) {
+    public Outfit(Top top, Bottom bottom, Shoes shoes, Jacket jacket, Full full)
+    {
         this.shoes = shoes;
         this.jacket = jacket;
-        if (full != null) {
+        if (full != null)
+        {
             this.full = full;
             this.top = null;
             this.bottom = null;
         }
-        else {
+        else
+        {
             this.top = top;
             this.bottom = bottom;
             this.full = null;
         }
     }
-    
+
+
     /**
      * Empty constructor for createOufits method in person.
      */
-    public Outfit() {
+    public Outfit()
+    {
     }
-    //~Public  Methods ........................................................
+
+
+    // ~Public Methods ........................................................
     /**
      * Gets the top item
      * 
      * @return top
      */
-    public Top getTop() {
+    public Top getTop()
+    {
         return top;
     }
+
+
     /**
      * Sets the top to the outfit, if full is set then null
      * 
-     * @param top top item to set
+     * @param top
+     *            top item to set
      */
-    public void setTop(Top top) {
+    public void setTop(Top top)
+    {
         this.top = top;
     }
+
+
     /**
      * Gets the bottom item
      * 
      * @return bottm
      */
-    public Bottom getBottom() {
+    public Bottom getBottom()
+    {
         return bottom;
     }
+
+
     /**
      * Sets the bottom item, if full is set then null
      * 
-     * @param bottom bottom item to set
+     * @param bottom
+     *            bottom item to set
      */
-    public void setBottom(Bottom bottom) {
+    public void setBottom(Bottom bottom)
+    {
         this.bottom = bottom;
     }
+
+
     /**
      * Gets the shoes
      * 
      * @return shoes
      */
-    public Shoes getShoes() {
+    public Shoes getShoes()
+    {
         return shoes;
     }
+
+
     /**
-     * Sets the shoes 
+     * Sets the shoes
      * 
-     * @param shoes shoes item to set 
+     * @param shoes
+     *            shoes item to set
      */
-    public void setShoes(Shoes shoes) {
+    public void setShoes(Shoes shoes)
+    {
         this.shoes = shoes;
     }
+
+
     /**
      * Gets the jacket item
      * 
      * @return jacket
      */
-    public Jacket getJacket() {
+    public Jacket getJacket()
+    {
         return jacket;
     }
+
+
     /**
-     * Sets the jacket 
+     * Sets the jacket
      * 
-     * @param jacket jacket item to set 
+     * @param jacket
+     *            jacket item to set
      */
-    public void setJacket(Jacket jacket) {
+    public void setJacket(Jacket jacket)
+    {
         this.jacket = jacket;
     }
+
+
     /**
-     * Gets the full body item 
+     * Gets the full body item
      * 
      * @return top
      */
-    public Full getFull() {
+    public Full getFull()
+    {
         return full;
     }
+
+
     /**
      * Sets the full to the outfit, if top and bottom are set then null
      * 
-     * @param full full body item to set 
+     * @param full
+     *            full body item to set
      */
-    public void setFull(Full full) {
+    public void setFull(Full full)
+    {
         this.full = full;
-        if (full != null) {
+        if (full != null)
+        {
             this.top = null;
             this.bottom = null;
         }
     }
+
+
     /**
-     * Builds string summary of the outfit 
+     * Builds string summary of the outfit
      * 
-     * @return outfit summary string 
+     * @return outfit summary string
      */
-    public String displayOutfit() {
+    public String displayOutfit()
+    {
         StringBuilder builder = new StringBuilder();
-        if (full != null) {
+        if (full != null)
+        {
             builder.append("Full: ").append(full.getDescription()).append("\n");
         }
-        else {
-            if(top != null) {
-                builder.append("Top: ").append(top.getDescription()).append("\n");
+        else
+        {
+            if (top != null)
+            {
+                builder.append("Top: ").append(top.getDescription())
+                    .append("\n");
             }
-            if(bottom != null) {
-                builder.append("Bottom: ").append(bottom.getDescription()).append("\n");
+            if (bottom != null)
+            {
+                builder.append("Bottom: ").append(bottom.getDescription())
+                    .append("\n");
             }
         }
-        if (jacket != null) {
-            builder.append("Jacket: ").append(jacket.getDescription()).append("\n");
+        if (jacket != null)
+        {
+            builder.append("Jacket: ").append(jacket.getDescription())
+                .append("\n");
         }
-        if (shoes != null) {
-            builder.append("Shoes: ").append(shoes.getDescription()).append("\n");
+        if (shoes != null)
+        {
+            builder.append("Shoes: ").append(shoes.getDescription())
+                .append("\n");
 
         }
         return builder.toString();
