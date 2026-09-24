8e761345f85408569409d51552224b8e1f0dc83e
sydneyang111 <sydneyangg@gmail.com>
2026-09-23 21:25:05 -0400

    edit shoes test

--- gearedGarmentGenerator/ShoesTest.java
+++ gearedGarmentGenerator/ShoesTest.java
@@ -1,10 +1,29 @@
 package gearedGarmentGenerator;
-public class ShoesTest
+import junit.framework.TestCase;
+/**
+ * // -------------------------------------------------------------------------
+/**
+ * Test the Shoes class
+ * 
+ *  @author sydneyyang
+ *  @version Sep 23, 2026
+ */
+public class ShoesTest extends TestCase
 {
-    //~ Fields ................................................................
-
-    //~ Constructors ..........................................................
-
-    //~Public  Methods ........................................................
+    private Shoes shoes;
+    /**
+     * setting up initial condition
+     */
+    public void setUp() {
+        shoes = new Shoes(false, "casual", "white sneakers");
+    }
+    /**
+     * tests setters and getters from shoes class
+     */ 
+    public void testShoesClass() {
+        assertFalse(shoes.getWarmth());
+        assertEquals("casual", shoes.getFormality());
+        assertEquals("white sneakers", shoes.getDescription());
+    }
 
 }
