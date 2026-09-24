3bcd2c96f1916900532c3071a129317a73dcb391
sydneyang111 <sydneyangg@gmail.com>
2026-09-23 20:19:45 -0400

    added full and shoes test and edited outfit

--- gearedGarmentGenerator/Outfit.java
+++ gearedGarmentGenerator/Outfit.java
@@ -63,7 +63,7 @@ public class Outfit
     /**
      * Gets the bottom item
      * 
-     * @return bottm
+     * @return bottom
      */
     public Bottom getBottom() {
         return bottom;
@@ -136,21 +136,21 @@ public class Outfit
     public String displayOutfit() {
         StringBuilder builder = new StringBuilder();
         if (full != null) {
-            builder.append("Full: ").append(full.Description().append("\n"));
+            builder.append("Full: ").append(full.getDescription().append("\n"));
         }
         else {
             if(top != null) {
-                builder.append("Top: ").append(top.Description().append("\n"));
+                builder.append("Top: ").append(top.getDescription().append("\n"));
             }
             if(bottom != null) {
-                builder.append("Bottom: ").append(bottom.Description().append("\n"));
+                builder.append("Bottom: ").append(bottom.getDescription().append("\n"));
             }
         }
         if (jacket != null) {
-            builder.append("Jacket: ").append(jacket.Description().append("\n"));
+            builder.append("Jacket: ").append(jacket.getDescription().append("\n"));
         }
         if (shoes != null) {
-            builder.append("Shoes: ").append(shoes.Description().append("\n"));
+            builder.append("Shoes: ").append(shoes.getDescription().append("\n"));
         }
         return builder.toString();
     }
--- gearedGarmentGenerator/ShoesTest.java
+++ gearedGarmentGenerator/ShoesTest.java
@@ -1,10 +1,31 @@
 package gearedGarmentGenerator;
-public class ShoesTest
+import junit.framework.TestCase;
+/**
+ * // -------------------------------------------------------------------------
+/**
+ *  Tests for Outfit class
+ * 
+ *  @author sydneyyang
+ *  @version Sep 22, 2026
+ */
+public class ShoesTest extends TestCase
 {
-    //~ Fields ................................................................
+    private Shoes shoes;
+    /**
+     * Sets up initial test conditions
+     */
+    public void setUp() {
+        shoes = new Shoes(false, "causal", "white sneakers");
+    }
+    
+    /**
+     * Tests constructor setters and getters
+     */
+    public void testShoeClass() {
+        assertFalse(shoes.getWarmth());
+        assertEquals("casual", shoes.getFormality());
+        assertEquals("white sneakers", shoes.getDescription());
+    }
 
-    //~ Constructors ..........................................................
-
-    //~Public  Methods ........................................................
 
 }
