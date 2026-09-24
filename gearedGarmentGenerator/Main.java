3d193fac66630e21056f1b90027432064f65b0d1
owenwikel <wikelogw@gmail.com>
2026-09-24 10:56:29 -0400

    final touches

--- gearedGarmentGenerator/Main.java
+++ gearedGarmentGenerator/Main.java
@@ -1,13 +1,23 @@
 package gearedGarmentGenerator;
+/**
+ * Starts the Geared Garment Generator.
+ * 
+ * @author Owen Wikel (wikelog)
+ * @version 2026.09.24
+ */
 public class Main {
     //~ Fields ................................................................
 
     //~ Constructors ..........................................................
-
-        public static void main(String[] args) {
-            Person user = Person.meetUser();
-            user.runProgram();
-        }
+        /**
+         * Creates a user and runs the oufit generator.
+         * 
+         * @param args unused
+         */
+    public static void main(String[] args) {
+        Person user = Person.meetUser();
+        user.runProgram();
+    }
     //~Public  Methods ........................................................
 
 }
